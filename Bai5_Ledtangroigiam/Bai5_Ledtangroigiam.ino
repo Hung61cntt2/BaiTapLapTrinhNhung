ba5bb9d55a94a7f9b429102a3c8fb9c0cca83c9d
Nguyễn Thành Hưng - 61131801 <hung.nt.61cntt@ntu.edu.vn>
2021-10-08 22:36:44 +0700

    first commit

---
+++ Bai5_Ledtangroigiam/Bai5_Ledtangroigiam.ino
@@ -0,0 +1,17 @@
+int i = 0;
+void setup() {
+   pinMode(9,OUTPUT);
+}
+void loop()
+{
+  for (i = 0; i<=255; i +=5)
+    {
+      analogWrite(9,i);
+      delay(1000);
+    }
+  for (i = 0; i<=255; i -=5)
+    {
+      analogWrite(9,i);
+      delay(1000);
+    }
+}
