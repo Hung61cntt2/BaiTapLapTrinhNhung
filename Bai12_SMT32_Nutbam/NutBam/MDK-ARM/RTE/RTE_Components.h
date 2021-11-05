3840d418b41f36ed766483703b951e224bf7c7a8
Nguyễn Thành Hưng - 61131801 <hung.nt.61cntt@ntu.edu.vn>
2021-11-05 19:38:53 +0700

    first commit

---
+++ Bai12_SMT32_Nutbam/NutBam/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'NutBam' 
+ * Target:  'NutBam' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
