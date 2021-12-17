aa3b173db55cda20a14c5e65c9b83d2883ddccd7
Nguyễn Thành Hưng - 61131801 <hung.nt.61cntt@ntu.edu.vn>
2021-12-17 16:59:12 +0700

    commit

---
+++ Bai19_CamBienMua/Bai19_CamBienMua.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int rainSensor = 6; // Chân tín hiệu cảm biến mưa ở chân digital 6 (arduino)
+
+void setup() {
+  pinMode(rainSensor, INPUT); // Đặt chân cảm biến mưa là INPUT, vì tín hiệu sẽ được truyền đến cho Arduino
+  Serial.begin(9600);// Khởi động Serial ở baudrate 9600
+  Serial.println("Da khoi dong xong");
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(3, 0);
+  lcd.print("Cam Bien Mua");
+}
+
+void loop() {
+  int value = digitalRead(rainSensor);//Đọc tín hiệu cảm biến mưa
+  if (value == LOW) { // Cảm biến đang không mưa
+    Serial.println("Đang không mưa");
+    lcd.setCursor(0, 1);
+    lcd.print("No rain");
+  } else {
+    Serial.println("Đang mưa");
+    lcd.setCursor(0, 1);
+    lcd.print("Raining");
+  }
+  delay(1000);
+}
