f9c0128d98f6a17013bbb2417690e5e5112ee6b6
Nguyễn Thành Hưng - 61131801 <hung.nt.61cntt@ntu.edu.vn>
2021-12-17 16:58:42 +0700

    commit

---
+++ Bai16_ULTRASONIC_LCD/Bai16_ULTRASONIC_LCD.ino
@@ -0,0 +1,41 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int trig = 8;
+const int echo = 7;
+//...............................................
+void setup()
+{
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Distance:");
+  lcd.setCursor(13, 0);
+  lcd.print(" cm");
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+//...............................................
+void loop()
+{
+  unsigned long duration;
+  int distance;
+  digitalWrite(trig, 0);
+  delayMicroseconds(2);
+  digitalWrite(trig, 1);
+  delayMicroseconds(5);
+  digitalWrite(trig, 0);
+  duration = pulseIn(echo, HIGH);
+  distance = int(duration / 2 / 29.412);
+  lcd.setCursor(10, 0);
+  lcd.print(distance);
+  if (distance < 10) {
+    lcd.setCursor(11, 0);
+    lcd.print("  ");
+  }
+  else if (distance < 100) {
+    lcd.setCursor(12, 0);
+    lcd.print(' ');
+  }
+  delay(200);
+}
